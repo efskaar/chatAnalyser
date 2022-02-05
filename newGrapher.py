a840383e3797b1fe6d267d8f06b12d1545be4ce3
efskaar <59534768+efskaar@users.noreply.github.com>
2022-02-05 23:44:36 +0100

    Update newGrapher.py
    Made the color picker repeat itself with %
    Toke the matplotlib settings in to the main function
    And removed the auto sorting data in makePlot

--- newGrapher.py
+++ newGrapher.py
@@ -6,9 +6,7 @@ import matplotlib
 #    'WXCairo', 'agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template']
 
 #makes emojis render proberly
-matplotlib.rcParams['font.size'] = 15
-matplotlib.rcParams["font.monospace"] = ["DejaVu Sans Mono"]
-matplotlib.rcParams["font.family"] = "monospace"
+
 
 
 class Grapher():
@@ -17,12 +15,18 @@ class Grapher():
       self.backend = {'png':'Agg','svg':'svg'}
       self.filetype = filetype
       matplotlib.use(self.backend[filetype])
-      self.goodColors = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#42d4f4', '#f032e6', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#000075', '#a9a9a9', '#ffffff', '#000000']
+      matplotlib.rcParams['font.size'] = 15
+      matplotlib.rcParams["font.monospace"] = ["DejaVu Sans Mono"]
+      matplotlib.rcParams["font.family"] = "monospace"
+      self.goodColors = ['#e6194B','#3cb44b','#ffe119','#4363d8','#f58231', 
+                          '#42d4f4','#f032e6','#fabed4','#469990','#dcbeff',
+                          '#9A6324','#fffac8','#800000','#aaffc3','#000075', 
+                          '#a9a9a9']
   
-  def makePlot(self,dict,filename,path='',border=False):
-    dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1])}
+  def makePlot(self,dict,filename,path=''):
     maxValue = max(dict.values())
     length = len(dict.keys())
+    colorLen = len(self.goodColors)
     keys = list(dict.keys())
     w = 3 #width
 
@@ -35,6 +39,7 @@ class Grapher():
       item.patch.set_visible(False)
     tick_params(top=False,right=False,left=False,bottom=False)
 
+    #making the bars and labels
     for i in range(length):
       key = keys[i]
       value = dict[key]
@@ -42,11 +47,15 @@ class Grapher():
       yStart = 0
       xEnd = ((1+i)*w) 
       yEnd = value/maxValue
-      ax.add_artist(Rectangle((xStart,yStart),w-0.5,yEnd,linewidth=3,fill=True,color=self.goodColors[i]))
-      text(xEnd+0.3,yEnd+0.05,f'{value}',rotation=0,fontsize=25,color=self.goodColors[i],ha="center",va="center",fontname='Segoe UI Emoji',fontweight="bold")
-      text(xEnd+0.3,-0.1,f'{key}',rotation=0,fontsize=30,color=self.goodColors[i],ha="center",va="center",fontname='Segoe UI Emoji')
+      ax.add_artist(Rectangle((xStart,yStart),w-0.5,yEnd,linewidth=3,fill=True,color=self.goodColors[i%colorLen]))
+      text(xEnd+0.3,yEnd+0.05,f'{value}',rotation=0,fontsize=25,color=self.goodColors[i%colorLen],ha="center",va="center",fontname='Segoe UI Emoji',fontweight="bold")
+      text(xEnd+0.3,-0.1,f'{key}',rotation=0,fontsize=30,color=self.goodColors[i%colorLen],ha="center",va="center",fontname='Segoe UI Emoji')
+    
+    #making it a tight fit to the bars
     xlim([0,length*w+w])
     ylim([-0.2,1.2])
+    
+    #removing the border created by the view and saving the file
     subplots_adjust(left=0,right=1, top=1, bottom=0.0) 
-    savefig(f'{path:s}{filename}-dpi{self.dpi:d}.{self.filetype}', dpi=self.dpi)
+    savefig(f'{path}{filename}-dpi{self.dpi:d}.{self.filetype}', dpi=self.dpi)
     close()
