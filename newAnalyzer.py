5ed391abd5987fe613fd3ee9cd75029ab3a4474c
efskaar <59534768+efskaar@users.noreply.github.com>
2022-02-05 23:46:20 +0100

    Update newAnalyzer.py
    Made new plot functions
    Made a full analysis function
    Made a function for auto creating the necessary directories

--- newAnalyzer.py
+++ newAnalyzer.py
@@ -1,11 +1,12 @@
 from newReadFile import Chat
 from newGrapher import Grapher
+import os
 
 class Analyser():
   def __init__(self,file):
-      self.rcf = Chat()
-      self.rcf.readFile(file)
-      self.grapher = Grapher()
+    self.rcf = Chat()
+    self.rcf.readFile(file)
+    self.grapher = Grapher()
 
   def totalReactionsInChat(self):
     people = self.rcf.people
@@ -21,21 +22,42 @@ class Analyser():
     return reactionsDict
 
   def plotDictData(self,func,name):
-    self.grapher.makePlot(func(),name)
+    self.grapher.makePlot(func(),name,path=self.rcf.chatName+'/')
 
   def plotEmojisGivenPerPerson(self):
     people = self.rcf.people
+    chatName = self.rcf.chatName
     for key in people.keys():
       p = people[key]
-      filename = f'{p}-given-'
-      self.grapher.makePlot(p.totalGivenReactions,filename,path='reactions\\')
+      filename = f'{chatName}/{p}/given-emojis'
+      self.grapher.makePlot(self.sortDict(p.totalGivenReactions),filename)
 
   def plotEmojisReceivedPerPerson(self):
     people = self.rcf.people
+    chatName = self.rcf.chatName
     for key in people.keys():
       p = people[key]
-      filename = f'{p}-gotten-'
-      self.grapher.makePlot(p.totalReactions,filename,path='reactions\\')
+      filename = f'{chatName}/{p}/gotten-emojis'
+      self.grapher.makePlot(self.sortDict(p.totalReactions),filename)
+  
+  def plotSendTimePerPerson(self):
+    people = self.rcf.people
+    chatName = self.rcf.chatName
+    for key in people.keys():
+      p = people[key]
+      data = p.timeSendMessageDict()
+      data = self.sortDict(data,False)
+      filename = f'{chatName}/{p}/sent-time'
+      self.grapher.makePlot(data,filename)
+
+  def plotSendDayOfWeekPerPerson(self):
+    people = self.rcf.people
+    chatName = self.rcf.chatName
+    for key in people.keys():
+      p = people[key]
+      data = p.dayOfWeekSendMessageDict()
+      filename = f'{chatName}/{p}/sent-dayWeek'
+      self.grapher.makePlot(data,filename)
 
   def printBasicInfo(self):
     people = self.rcf.people
@@ -51,10 +73,42 @@ class Analyser():
       summen += sum(p.totalReactions.values())
     print('\n\n\n')
     print(self.totalReactionsInChat())
+  
+  def fullAnalysisAndDataCreation(self):
+    '''
+    In case you want a full analysis of the chat
+    '''
+    self.makeNecessaryDirs()
+    self.plotDictData(self.totalReactionsInChat,"emojis")
+    self.plotEmojisGivenPerPerson()
+    self.plotEmojisReceivedPerPerson()
+    # self.printBasicInfo()
+
+  def sortDict(self,data,isValue=True):
+    return {k: v for k, v in sorted(data.items(), key=lambda item: item[isValue])}
+
+  def makeDir(self,path):
+    '''
+    For making a directory
+    '''
+    if not os.path.exists(path):
+      os.mkdir(path)
+      print("Directory " , path ,  " Created ")
+    # else:    
+    #   print("Directory " , path ,  " already exists")
+  
+  def makeNecessaryDirs(self):
+    '''
+    Makes all necessary directories for the results files
+    '''
+    people = self.rcf.people
+    chatName = self.rcf.chatName
+    self.makeDir(chatName)
+    for p in people:
+      self.makeDir(chatName+'/'+str(p))
 
 if '__main__' == __name__:
   analyzer = Analyser('tfn.html')
-  analyzer.plotDictData(analyzer.totalReactionsInChat,"emojis")
-  analyzer.plotEmojisGivenPerPerson()
-  analyzer.plotEmojisReceivedPerPerson()
-  analyzer.printBasicInfo()
+  # analyzer.makeNecessaryDirs()
+  analyzer.plotSendDayOfWeekPerPerson()
+  # analyzer.fullAnalysisAndDataCreation()
