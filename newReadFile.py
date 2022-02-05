3957def520660da022626afacfc018c930103a25
efskaar <59534768+efskaar@users.noreply.github.com>
2022-02-05 16:24:55 +0100

    Update newReadFile.py
    removed prints from the file
    The analyzer part has been moved to newAnalyzer.py

--- newReadFile.py
+++ newReadFile.py
@@ -111,19 +111,4 @@ class Chat():
 if '__main__' == __name__:
   rcf = Chat()
   rcf.readFile('tfn.html')
-  people = rcf.people
-  summen = 0
-  for name in people.keys():
-    # print('\n\n',name)
-    p = people[name]
-    words = p.words
-    # words = {k: v for k, v in sorted(p.words.items(), key=lambda item: item[1])}
-    # for word in words:
-    #   print(word,words[word])
-    print('\n',name)
-    print(f'{len(p.messages)} messages with {sum(words.values())} words')
-    print(f'On average {sum(words.values())/len(p.messages):.0f} words per message')
-    print('Received',sum(p.totalReactions.values()),p.totalReactions)
-    print('Given',sum(p.totalGivenReactions.values()),p.totalGivenReactions)
-    summen += sum(p.totalReactions.values())
     
